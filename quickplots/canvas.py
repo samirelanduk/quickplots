087edb11d466bb3d2061a0df37b9fe6e3d349a57
Sam Ireland <sam.ireland.uk@gmail.com>
2016-02-06 17:29:32 +0000

    Move request_repaint

--- quickplots/canvas.py
+++ quickplots/canvas.py
@@ -3,3 +3,18 @@ class QuickplotsCanvas:
 
     def __init__(self, chart):
         self.chart = chart
+
+
+    def request_repaint(self):
+        self.chart._prepare_canvas()
+        self.chart._paint_grids()
+        self.chart._paint_series()
+        self.chart._paint_plot_bounds()
+        self.chart._write_title()
+        self.chart._write_legend_labels()
+        self.chart._paint_legend_symbols()
+        self.chart._paint_debug_lines()
+
+
+    def draw_to_tkinter(self, tkinter_canvas):
+        pass
