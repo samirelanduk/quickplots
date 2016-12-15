03a9dda3743b7a394bb8c60e62d5938b03b55907
Sam Ireland <sam.ireland.uk@gmail.com>
2016-12-15 17:39:08 +0000

    Add option to give series data as x and y lists

--- quickplots/series.py
+++ quickplots/series.py
@@ -1,4 +1,7 @@
 class Series:
 
     def __init__(self, *data):
-        self._data = list(data)
+        if len(data) == 2:
+            self._data = list(zip(data[0], data[1]))
+        else:
+            self._data = list(data)
--- tests/test_series.py
+++ tests/test_series.py
@@ -4,5 +4,10 @@ from quickplots.series import Series
 class SeriesCreationTests(TestCase):
 
     def test_can_create_series(self):
-        series = Series((1,1), (2, 4), (3, 9))
-        self.assertEqual(series._data, [(1,1), (2, 4), (3, 9)])
+        series = Series((1, 1), (2, 4), (3, 9))
+        self.assertEqual(series._data, [(1, 1), (2, 4), (3, 9)])
+
+
+    def test_can_create_series_as_seperate_x_and_y_values(self):
+        series = Series((1, 2, 3), (1, 4, 9))
+        self.assertEqual(series._data, [(1, 1), (2, 4), (3, 9)])
