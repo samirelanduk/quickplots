817bbc3c7ebd1e92ff6eb57fbb3cae9e79b2f02b
Sam Ireland <sam.ireland.uk@gmail.com>
2016-12-15 17:42:21 +0000

    Ensure default intepretatiom is (x,y) when creating series

--- quickplots/series.py
+++ quickplots/series.py
@@ -1,7 +1,7 @@
 class Series:
 
     def __init__(self, *data):
-        if len(data) == 2:
-            self._data = list(zip(data[0], data[1]))
-        else:
+        if len(data) > 2 or len(data[0]) == 2:
             self._data = list(data)
+        else:
+            self._data = list(zip(data[0], data[1]))
--- tests/test_series.py
+++ tests/test_series.py
@@ -11,3 +11,8 @@ class SeriesCreationTests(TestCase):
     def test_can_create_series_as_seperate_x_and_y_values(self):
         series = Series((1, 2, 3), (1, 4, 9))
         self.assertEqual(series._data, [(1, 1), (2, 4), (3, 9)])
+
+
+    def test_data_interpreted_as_xy_points_when_ambiguous(self):
+        series = Series((1, 1), (2, 4))
+        self.assertEqual(series._data, [(1, 1), (2, 4)])
