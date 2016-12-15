2aab03fc14493923452cdf616708a3af650851c4
Sam Ireland <sam.ireland.uk@gmail.com>
2016-12-15 17:48:53 +0000

    Add requirement for data to be as lists or tuples

--- quickplots/series.py
+++ quickplots/series.py
@@ -1,7 +1,11 @@
 class Series:
 
     def __init__(self, *data):
-        if len(data) > 2 or len(data[0]) == 2:
-            self._data = list(data)
-        else:
-            self._data = list(zip(data[0], data[1]))
+        self._data = []
+        for point in data:
+            if not isinstance(point, list) and not isinstance(point, tuple):
+                raise TypeError(
+                 "Data must be give as lists or tuples, not '%s'" % str(point)
+                )
+            else:
+                self._data.append(tuple(point))
--- tests/test_series.py
+++ tests/test_series.py
@@ -8,11 +8,21 @@ class SeriesCreationTests(TestCase):
         self.assertEqual(series._data, [(1, 1), (2, 4), (3, 9)])
 
 
-    def test_can_create_series_as_seperate_x_and_y_values(self):
+    def test_can_create_series_with_list_points(self):
+        series = Series([1, 1], (2, 4), [3, 9])
+        self.assertEqual(series._data, [(1, 1), (2, 4), (3, 9)])
+
+
+    def test_points_can_only_be_list_or_tuple(self):
+        with self.assertRaises(TypeError):
+            Series([1, 1], (2, 4), [3, 9], set([8, 9]))
+
+
+    '''def test_can_create_series_as_seperate_x_and_y_values(self):
         series = Series((1, 2, 3), (1, 4, 9))
         self.assertEqual(series._data, [(1, 1), (2, 4), (3, 9)])
 
 
     def test_data_interpreted_as_xy_points_when_ambiguous(self):
         series = Series((1, 1), (2, 4))
-        self.assertEqual(series._data, [(1, 1), (2, 4)])
+        self.assertEqual(series._data, [(1, 1), (2, 4)])'''
