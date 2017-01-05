f69b24e673cc5b0ce838ad23d8b89973dafb6e97
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-05 03:16:12 +0000

    line method will take chart properties

--- quickplots/quick.py
+++ quickplots/quick.py
@@ -2,6 +2,11 @@ from .series import LineSeries
 from .charts import AxisChart
 
 def line(*args, **kwargs):
-    series = LineSeries(*args, **kwargs)
-    chart = AxisChart(series)
+    line_series_kwargs = {}
+    for kwarg in ("name", "color", "linestyle"):
+        if kwarg in kwargs:
+            line_series_kwargs[kwarg] = kwargs[kwarg]
+            del kwargs[kwarg]
+    series = LineSeries(*args, **line_series_kwargs)
+    chart = AxisChart(series, **kwargs)
     return chart
--- tests/test_quick_chart_functions.py
+++ tests/test_quick_chart_functions.py
@@ -10,6 +10,24 @@ class LineChartCreationTests(TestCase):
 
 
     def test_can_create_line_chart_with_line_properties(self):
-        chart = line((1, 1), (2, 4), (3, 9), color="#000099", linestyle="--")
+        chart = line(
+         (1, 1), (2, 4), (3, 9), name="squares", color="#000099", linestyle="--"
+        )
+        self.assertEqual(chart.series().name(), "squares")
         self.assertEqual(chart.series().color(), "#000099")
         self.assertEqual(chart.series().linestyle(), "--")
+
+
+    def test_can_create_line_chart_with_chart_properties(self):
+        chart = line(
+         (1, 1), (2, 4), (3, 9),
+         name="squares", color="#000099", linestyle="--",
+         title="Title", width=900, x_label="xxx", y_label="yyy"
+        )
+        self.assertEqual(chart.series().name(), "squares")
+        self.assertEqual(chart.series().color(), "#000099")
+        self.assertEqual(chart.series().linestyle(), "--")
+        self.assertEqual(chart.title(), "Title")
+        self.assertEqual(chart.width(), 900)
+        self.assertEqual(chart.x_label(), "xxx")
+        self.assertEqual(chart.y_label(), "yyy")
